265d8203b8c9df0d67f3fea8122a61b8af219ed2
johnchower <johnchower@gmail.com>
2016-10-04 16:41:03 -0600

    Set up test_timeline data set and translation function

---
+++ R/data.r
@@ -0,0 +1,16 @@
+#' Hypothetical timeline of space and timeline post actions
+#'
+#' A dataset containing a small timeline of posting, commenting, and sharing
+#' actions taken by hypothetical users. Used for testing the data processing
+#' functions included in the gloograph package.
+#'
+#' @format
+#' \describe{
+#'   \item{time}{When the action was taken.}
+#'   \item{subject}{The user who took a specified action}
+#'   \item{action}{What the user did}
+#'   \item{object}{Who the user did the action to}
+#'   \item{post_id}{In the case of a post oject, the id of the post}
+#' }
+
+"test_timeline"
---
+++ R/translate_data.r
@@ -0,0 +1,1 @@
+#' Translate data to prep it for the engagement score calculation 
