88c0afb8631da7252aa76653882ed5aa390b7aba
johnchower <johnchower@gmail.com>
2016-10-04 17:03:24 -0600

    Create skeleton of the test_data creator function.

---
+++ R/create_test_data.r
@@ -0,0 +1,41 @@
+# Functions to create test data sets
+
+#' Create 'calculated' test data set.
+#'
+#' @param timeline A data frame that describes a sequence of post-related
+#' actions.
+#' @return A data frame consisting of two columns: post_id engagement_score.
+
+create_calculated_test_data <- function(timeline){
+  "hello"
+}
+
+#' Create 'translated' test data set.
+#'
+#' @param timeline A data frame that describes a sequence of post-related
+#' actions.
+#' @return
+
+create_translated_test_data <- function(timeline){
+  "hello"
+}
+
+#' Create 'organized' test data.
+#'
+#' @param timeline A data frame that describes a sequence of post-related
+#' actions.
+#' @return
+
+create_organized_test_data <- function(timeline){
+  "hello"
+}
+
+#' Create 'pulled' test data.
+#'
+#' @param timeline A data frame that describes a sequence of post-related
+#' actions.
+#' @return
+
+create_pulled_test_data <- function(timeline){
+  "hello"
+}
